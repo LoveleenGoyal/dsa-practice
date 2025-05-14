7e48df71bcc69062cdb4c18d067c669d8f1559cf
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-05-14 09:50:39 +0530

    Peak Index in a mountain array

---
+++ PeakIndex.java
@@ -0,0 +1,30 @@
+class PeakIndex {
+    public int peakIndexInMountainArray(int[] arr) {
+        // int low = 0;
+        // int high = arr.length - 1;
+        // while(low < high) {
+        //     int mid = low + (high - low) / 2;
+        //     if (arr[mid] > arr[mid + 1]) {
+        //         high = mid;
+        //     } else {
+        //         low = mid + 1;
+        //     }
+        // }
+        // return low;
+        return binarySearchPeak(arr, 0, arr.length - 1);
+    }
+
+    private int binarySearchPeak(int[] arr, int low, int high) {
+        if (low == high) {
+            return low;
+        }
+        int mid = low + (high - low) / 2;
+        if(arr[mid] > arr[mid + 1]) {
+            return binarySearchPeak(arr, low, mid);
+        }
+        else {
+            return binarySearchPeak(arr, mid + 1, high);
+        }
+
+    }
+}
